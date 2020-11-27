c25f3bcbb7ad6d49259b39958b2629418f501635
Xu <nhathuyhedspi@gmail.com>
2020-11-27 02:15:56 +0000

    Add detail book

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_151658) do
+ActiveRecord::Schema.define(version: 2020_11_27_015825) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2020_11_25_151658) do
     t.integer "number_of_pages"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "detail"
   end
 
   create_table "images", force: :cascade do |t|
